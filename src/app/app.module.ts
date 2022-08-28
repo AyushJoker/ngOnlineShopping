431058edbfd91607665d0781ba24756f44457a6c
AyushJoker <ayushjoker9@gmail.com>
2022-08-28 19:52:44 +0530

    changes home page and all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
 import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-
-
 import { OrdersComponent } from './orders/orders.component';
 import { CartComponent } from './cart/cart.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ProductsingleComponent } from './productsingle/productsingle.component';
 import { ProductComponent } from './product/product.component';
 import { CatwiseproductComponent } from './catwiseproduct/catwiseproduct.component';
-//import { CategoryComponent } from './category/category.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -47,7 +44,6 @@ import { CatwiseproductComponent } from './catwiseproduct/catwiseproduct.compone
     FontAwesomeModule,
     AppRoutingModule,
     BrowserModule,
-    SlickCarouselModule,
     FormsModule,
     HttpClientModule
   ],
